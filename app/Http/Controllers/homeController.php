d5d188c0838357ca8d9b3895cc54169eab3c9cea
Lugh-o <lughfalcao@gmail.com>
2024-06-14 12:40:55 -0300

    Pequenas Correções no Commit anterior

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
     public function index(){
         return view("home.index");
--- app/Http/Controllers/saibamaisController.php
+++ app/Http/Controllers/saibamaisController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class saibamaisController extends Controller
+class SaibamaisController extends Controller
 {
     public function index(){
         return view("home.saibamais");
