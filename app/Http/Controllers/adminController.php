1fa6489e20ea7290526a870a3e2bfa4b1fc91435
KenzoVictor <105060117+KenzoVictor@users.noreply.github.com>
2024-07-15 20:43:49 -0300

    Controller Admin

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    public function index(){
+        return view("admin.index");
+    }
+
+    public function create(){
+        return view("admin.usuarioCad");
+    }
+}
