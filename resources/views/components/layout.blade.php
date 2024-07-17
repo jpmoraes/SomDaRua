92de09a9eeff069113b0d4e8aba319a6653d1cc4
Gabriel Pereira Araujo <gabriel53720436@aluno.ba.senac.br>
2024-07-16 21:13:27 -0300

    Atualização do fronent 1.1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
+  <link rel="icon" href="resources/assets/som_rua_icone.ico" sizes="16x16" type="image/x-icon">
   <link rel="stylesheet" href="{{asset($css)}}"/>
   <link rel="stylesheet" href="css/footer.css">
   <link rel="stylesheet" href="css/header.css">
@@ -68,6 +68,7 @@
   
 
   {{$slot}}
+ 
 
   <footer class="footer">
     <div class="footer_container">
