059fccf34ff72dd52500b4df73302796cedc12b5
AntaoNeto <antao.s.s.neto@live.com>
2024-07-17 21:09:03 -0300

    Card Home

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
 use Illuminate\Http\Request;
+use App\Models\Genero;
+use App\Models\GeneroEvento;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view("home.index");
+    public function index()
+    {
+        $dataAtual = Carbon::now();
+    $dataLimite = $dataAtual->copy()->addDays(60);
+
+    $eventosArray = Evento::whereDate('data', '>=', $dataAtual)
+        ->whereDate('data', '<=', $dataLimite)
+        ->orderBy('data', 'asc')
+        ->get(['id_evento', 'nome', 'data', 'hora']);
+
+    foreach ($eventosArray as &$evento) {
+        $generos = GeneroEvento::where('evento_id', $evento['id_evento'])
+            ->join('genero', 'genero.id_genero', '=', 'genero_evento.genero_id')
+            ->pluck('genero.nome')
+            ->toArray();
+
+        $evento['generos'] = $generos;
+    }
+
+    return view("home.index")->with('eventosArray', $eventosArray);
     }
 }
---
+++ resources/views/components/cardEventoHome.blade.php
@@ -0,0 +1,10 @@
+<div class="card" style="width: 18rem;" id="icardEvento">
+    <div class="card-body" id="icardBody">
+        <h5 class="card-title" id="icardTitle">{{$nome}}</h5>
+    </div>
+    <ul class="list-group list-group-flush" id="icardul">
+        <li class="list-group-item" id="icardli">{{$data}}</li>
+        <li class="list-group-item" id="icardli">{{$genero}}</li>
+        <li class="list-group-item" id="icardli">{{$hora}}</li>
+    </ul>
+</div>
